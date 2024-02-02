f9a5b71f46e662af35a801515edc26572a59871f
Naseem780 <mhdnaseem32@gmail.com>
2024-02-03 01:20:29 +0530

    Responsive Navigation Bar

---
+++ frontend/react/src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css"
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+function Navbar({clicked, isClicked}) {
+    const handleClicked = () => {
+        isClicked(!clicked);
+        console.log("clicked")
+      };
+  return (
+    <div className='Nav'>
+      
+      <ul className='NavbarWrapper'>
+
+        <li className='NavLogo'><Link to="/" className='Link'>Quiz Generator</Link></li>
+        <li className='NavElements'><NavLink to="/" className='Link'>Home</NavLink></li>
+        <li className='NavElements'><NavLink to="/about-us" className='Link'>About us</NavLink></li>
+        <li className='NavElements'><NavLink to="/contact-us" className='Link'>Contact Us</NavLink></li>
+        <li className='NavButton'><NavLink to="/sign-up"className='Link'>Sign up</NavLink></li>
+        <li className='NavElements'><NavLink to="/sign-in" className='Link'>Sign In</NavLink></li>
+
+      </ul>
+      {!clicked ? (
+        <GiHamburgerMenu onClick={handleClicked} className="Icon" />
+      ) : (
+        <ImCross onClick={handleClicked} className="Icon" />
+      )}
+    </div>
+  )
+}
+
+export default Navbar
