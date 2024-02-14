e8a65733e7d84f3d33ce6d8b0930bfeff6812a9b
aaqilruzzan <aaqil.ruzzan@gmail.com>
2024-02-14 13:47:30 +0530

    update

---
+++ frontend/react/src/Pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div style={{textAlign:'center', paddingTop:"300px"}}>
+      AboutUs
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ frontend/react/src/Pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div style={{textAlign:'center', paddingTop:"300px"}}>
+      ContactUs
+    </div>
+  )
+}
+
+export default ContactUs
