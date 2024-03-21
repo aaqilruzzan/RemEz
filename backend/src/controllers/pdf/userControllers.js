e0ef771cad1a80eadf1ea0d83ecd4df9ca93001f
Ruvinda Nirmal <ruvinda.nirmal1@gmail.com>
2024-03-21 20:49:48 +0530

    Requre_add

--- backend/src/controllers/pdf/userControllers.js
+++ backend/src/controllers/pdf/userControllers.js
@@ -1,40 +1,40 @@
 const User = require('../models/userModel')
-const jwt = requiire('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
 }
 
-//login user
-const loginUser = async (req,res) => {
-    const {email, password} = req.body
+// login a user
+const loginUser = async (req, res) => {
+  const {email, password} = req.body
 
-    try{
-        const user = await User.login(email, passsword)
+  try {
+    const user = await User.login(email, password)
 
-        // create token 
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({email, token})
-    }catch(error){
-        res.status(400).json({error: error.massage})
-    }
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
-//signup user
-const signupUser = async (req,res) => {
-    const {email, password} = req.body
+// signup a user
+const signupUser = async (req, res) => {
+  const {email, password} = req.body
 
-    try{
-        const user = await User.signup(email, passsword)
+  try {
+    const user = await User.signup(email, password)
 
-        // create token 
-        const token = createToken(user._id)
+    // create a token
+    const token = createToken(user._id)
 
-        res.status(200).json({email, token})
-    }catch(error){
-        res.status(400).json({error: error.massage})
-    }
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
-module.exports = {loginUser,signupUser}
+module.exports = { signupUser, loginUser }
