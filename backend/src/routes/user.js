c4ad4f65316da4c1c770a4fc4b43aaf31e08380e
Ruvinda Nirmal <ruvinda.nirmal1@gmail.com>
2024-03-21 20:58:26 +0530

    final_B-end_JWT

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,15 +1,14 @@
 const express = require('express')
 
-//controller function
-const{loginUser,signupUser} = require('../controllers/pdf/userControllers')
+// controller functions
+const { loginUser, signupUser } = require('../controllers/userController')
 
 const router = express.Router()
 
-//login route
-router.post('/login' , loginUser)
+// login route
+router.post('/login', loginUser)
 
-//signup login
-router.post('/signup' , signupUser)
-
+// signup route
+router.post('/signup', signupUser)
 
 module.exports = router
