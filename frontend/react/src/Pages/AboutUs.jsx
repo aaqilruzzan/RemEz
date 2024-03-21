b02b7dad4b3bc6fd7607dfb928d93c0645a44d4c
Naseem780 <mhdnaseem32@gmail.com>
2024-03-21 20:04:14 +0530

    final about us page

--- frontend/react/src/Pages/AboutUs.jsx
+++ frontend/react/src/Pages/AboutUs.jsx
@@ -12,7 +12,8 @@ function AboutUs() {
               <div className='card'>
                 <img alt="Gold" src='person1.png' className='card-image'></img>
                 <h2 className='card-title'>Naseem</h2>
-                <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering</p>
+                <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering </p>
+                <p className='card-text'> Second Year</p>
                 <p className='score'>IIT</p>
               </div>
 
@@ -20,6 +21,7 @@ function AboutUs() {
                 <img alt="Gold" src="person2.jpeg" className='card-image'></img>
                 <h2 className='card-title'>Anuda</h2>
                 <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering</p>
+                <p className='card-text'> Second Year</p>
                 <p className='score'>IIT</p>
               </div>
 
@@ -27,6 +29,7 @@ function AboutUs() {
                 <img alt="Gold" src="person3.jpeg" className='card-image'></img>
                 <h2 className='card-title'>Ruvinda</h2>
                 <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering</p>
+                <p className='card-text'> Second Year</p>
                 <p className='score'>IIT</p>
               </div>
 
@@ -35,6 +38,7 @@ function AboutUs() {
                 <img alt="Gold" src='person4.jpg' className='card-image'></img>
                 <h2 className='card-title'>Aaqil</h2>
                 <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering</p>
+                <p className='card-text'> Second Year</p>
                 <p className='score'>IIT</p>
               </div>
 
@@ -42,6 +46,7 @@ function AboutUs() {
                 <img alt="Gold" src="person5.jpeg" className='card-image'></img>
                 <h2 className='card-title'>Shimar</h2>
                 <p className='card-text'>Bachelor of Honors (B.Hons) degree in Software Engineering</p>
+                <p className='card-text'> Second Year</p>
                 <p className='score'>IIT</p>
               </div>
 
