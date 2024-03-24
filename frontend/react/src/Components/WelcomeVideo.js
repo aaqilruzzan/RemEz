203137b932a15739aeb05524af1ccc80d92ce1ce
aaqilruzzan <aaqil.ruzzan@gmail.com>
2024-03-24 17:25:46 +0530

    welcome video css deployment fix attempt 3

--- frontend/react/src/Components/WelcomeVideo.js
+++ frontend/react/src/Components/WelcomeVideo.js
@@ -3,7 +3,7 @@ import "./WelcomeVideo.css";
 const WelcomeVideo = () => {
   return (
     <div className="welcome-video-container">
-      <video autoPlay muted loop width="680" height="400">
+      <video autoPlay muted loop height="400">
         <source src="/loading.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
